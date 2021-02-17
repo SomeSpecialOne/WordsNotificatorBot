5e3c9bac9796cecd6b8e1a5277d029856bcf527d
SomeSpecialOne <tkachenkodmitriy@yahoo.com>
2021-02-17 19:17:44 +0300

    Fix good morning stickers, relocate methods to User class. Added sleep time check

--- NotificatorClass.py
+++ NotificatorClass.py
@@ -25,19 +25,19 @@ stickers_good_night = [
 ]
 
 stickers_good_morning = [
-    "CAACAgIAAxkBAAIbP2Aq283vLmD8Q1qoC2f6NhafOxRjAAKaAAP3AsgP0dUG8v161DgeBA"
-    "CAACAgIAAxkBAAIbQGAq29GNRTbOC7AXPgAB1a3CzL6_yAACEQMAAvPjvgsZbp8lnswsJB4E"
-    "CAACAgIAAxkBAAIbQWAq29Q39TJyVTOUUQRpM1to_azNAAIeCQACGELuCPlY2e4dIZwhHgQ"
-    "CAACAgIAAxkBAAIbQmAq29vYYXNJdRYooNKNoD2F6QABSQACVQIAAladvQqsSyyCT6MV3x4E"
-    "CAACAgIAAxkBAAIbQ2Aq2-mOT94OsuDY4PEvZ3Wahw06AAIEBwACRvusBEPY1ijiqT2DHgQ"
-    "CAACAgIAAxkBAAIbRGAq2_FmveURmfaJ_zOjd-sV7v9HAAJdAANEDc8Xx77nXM4YAjweBA"
-    "CAACAgIAAxkBAAIbRWAq2_MTC9tDbxHQIPy2gijDYRgSAAJBAAOvxlEakmwMdNAnIj4eBA"
-    "CAACAgIAAxkBAAIbRmAq2_ay6mGWxELH072OQHRJLDiwAAJuBQACP5XMCoY62V2IvLc1HgQ"
-    "CAACAgEAAxkBAAIbR2Aq2_2gHPbDLlp7OL7MTHL4e6dbAAIPAQACOA6CEbXMTX_iFJGCHgQ"
-    "CAACAgIAAxkBAAIbSGAq3AiVwPFMfmxsx0OmHoQVE3ozAAKSAQACVp29Cp_QLQhCLtUFHgQ"
-    "CAACAgIAAxkBAAIbSWAq3At42p5z5A2TjcEjRJp6RBYiAAJcAANZu_wlj1s_8uLbhRAeBA"
-    "CAACAgIAAxkBAAIbSmAq3B3Y6igPhu_StA5sspQw4lQcAAKCAAOmysgMnFoH8pxBBGceBA"
-    "CAACAgIAAxkBAAIbS2Aq3C-fBSFgDDRg3vyWdDnVT1bTAAIcAAP3AsgPcBxyLW2TGHgeBA"
+    "CAACAgIAAxkBAAIbP2Aq283vLmD8Q1qoC2f6NhafOxRjAAKaAAP3AsgP0dUG8v161DgeBA",
+    "CAACAgIAAxkBAAIbQGAq29GNRTbOC7AXPgAB1a3CzL6_yAACEQMAAvPjvgsZbp8lnswsJB4E",
+    "CAACAgIAAxkBAAIbQWAq29Q39TJyVTOUUQRpM1to_azNAAIeCQACGELuCPlY2e4dIZwhHgQ",
+    "CAACAgIAAxkBAAIbQmAq29vYYXNJdRYooNKNoD2F6QABSQACVQIAAladvQqsSyyCT6MV3x4E",
+    "CAACAgIAAxkBAAIbQ2Aq2-mOT94OsuDY4PEvZ3Wahw06AAIEBwACRvusBEPY1ijiqT2DHgQ",
+    "CAACAgIAAxkBAAIbRGAq2_FmveURmfaJ_zOjd-sV7v9HAAJdAANEDc8Xx77nXM4YAjweBA",
+    "CAACAgIAAxkBAAIbRWAq2_MTC9tDbxHQIPy2gijDYRgSAAJBAAOvxlEakmwMdNAnIj4eBA",
+    "CAACAgIAAxkBAAIbRmAq2_ay6mGWxELH072OQHRJLDiwAAJuBQACP5XMCoY62V2IvLc1HgQ",
+    "CAACAgEAAxkBAAIbR2Aq2_2gHPbDLlp7OL7MTHL4e6dbAAIPAQACOA6CEbXMTX_iFJGCHgQ",
+    "CAACAgIAAxkBAAIbSGAq3AiVwPFMfmxsx0OmHoQVE3ozAAKSAQACVp29Cp_QLQhCLtUFHgQ",
+    "CAACAgIAAxkBAAIbSWAq3At42p5z5A2TjcEjRJp6RBYiAAJcAANZu_wlj1s_8uLbhRAeBA",
+    "CAACAgIAAxkBAAIbSmAq3B3Y6igPhu_StA5sspQw4lQcAAKCAAOmysgMnFoH8pxBBGceBA",
+    "CAACAgIAAxkBAAIbS2Aq3C-fBSFgDDRg3vyWdDnVT1bTAAIcAAP3AsgPcBxyLW2TGHgeBA",
     "CAACAgIAAxkBAAIbTGAq3Dd2PXrSCs4Xetari0lWjhogAAJuAAPANk8TbYftSrN4mZceBA"
 ]
 
@@ -53,6 +53,48 @@ class User:
         self.period = period
 
         self.word = word
+
+    def time_check(self) -> bool:
+        """
+        Checking if current time is True or False to send message
+        """
+        current_hour = time.gmtime(time.time() + (3600 * self.time_zone)).tm_hour
+        if self.sleep_from > self.sleep_to:
+            if current_hour in range(self.sleep_to, self.sleep_from + 1):
+                return True
+            else:
+                return False
+        else:
+            if current_hour not in range(self.sleep_from + 1, self.sleep_to):
+                return True
+            else:
+                return False
+
+    def sleep_time_check(self):
+        """
+        Checking if current sending is last for this day to send good night sticker
+        """
+        if self.period == 3600:
+            return False
+        elif self.period == 7200:
+            current_hour = time.gmtime(time.time() + (3600 * self.time_zone)).tm_hour + 2
+        else:
+            current_hour = time.gmtime(time.time() + (3600 * self.time_zone)).tm_hour + 3
+        if self.sleep_from > self.sleep_to:
+            if current_hour in range(self.sleep_to, self.sleep_from + 1):
+                return False
+            else:
+                return True
+        else:
+            if current_hour not in range(self.sleep_from + 1, self.sleep_to):
+                return False
+            else:
+                return True
+
+    def logging(self, text: Exception):
+        with open('logs.txt', 'a') as logs:
+            logs.write(
+                f'[{time.strftime("%H:%M", time.gmtime(time.time() + (3600 * 3)))}] {text} for {self.first_name} [{self.sleep_from}-{self.sleep_to}, {self.period}]\n')
 
     class Word:
         def __init__(self, id: int, word: str, translation: str, context_en, context_ru: str):
@@ -97,17 +139,17 @@ class Notificator:
         for user in self.users:
             current_hour = time.gmtime(time.time() + (3600 * user.time_zone)).tm_hour
             if user.period == 3600:
-                if self.time_check(user) is True:
-                    if current_hour == user.sleep_from:
+                if user.time_check() is True:
+                    if current_hour == user.sleep_from or user.sleep_time_check():
                         try:
                             await bot.send_sticker(chat_id=user.id, sticker=random.choice(stickers_good_night))
                         except Exception as e:
-                            self.logging(e, user)
+                            user.logging(e)
                     elif current_hour == user.sleep_to:
                         try:
                             await bot.send_sticker(chat_id=user.id, sticker=random.choice(stickers_good_morning))
                         except Exception as e:
-                            self.logging(e, user)
+                            user.logging(e)
                     msg_word = f'<b>{user.word.word}</b> 🇬🇧 - <b>{user.word.translation}</b> 🇷🇺'
                     msg_context = f'{user.word.context_en} 🇬🇧\n' \
                                   f'{user.word.context_ru} 🇷🇺'
@@ -117,21 +159,21 @@ class Notificator:
                         await bot.send_message(chat_id=user.id, text=msg_word, parse_mode='HTML')
                         await bot.send_message(chat_id=user.id, text=msg_context, parse_mode='HTML')
                     except Exception as e:
-                        self.logging(e, user)
+                        user.logging(e)
 
             elif user.period == 7200:
-                if self.time_check(user) is True:
+                if user.time_check() is True:
                     if (user.sleep_to - current_hour) % 2 == 0:
-                        if current_hour == user.sleep_from:
+                        if current_hour == user.sleep_from or user.sleep_time_check():
                             try:
                                 await bot.send_sticker(chat_id=user.id, sticker=random.choice(stickers_good_night))
                             except Exception as e:
-                                self.logging(e, user)
+                                user.logging(e)
                         elif current_hour == user.sleep_to:
                             try:
                                 await bot.send_sticker(chat_id=user.id, sticker=random.choice(stickers_good_morning))
                             except Exception as e:
-                                self.logging(e, user)
+                                user.logging(e)
                         msg_word = f'<b>{user.word.word}</b> 🇬🇧 - <b>{user.word.translation}</b> 🇷🇺'
                         msg_context = f'{user.word.context_en} 🇬🇧\n' \
                                       f'{user.word.context_ru} 🇷🇺'
@@ -141,21 +183,21 @@ class Notificator:
                             await bot.send_message(chat_id=user.id, text=msg_word, parse_mode='HTML')
                             await bot.send_message(chat_id=user.id, text=msg_context, parse_mode='HTML')
                         except Exception as e:
-                            self.logging(e, user)
+                            user.logging(e)
 
             elif user.period == 10800:
-                if self.time_check(user) is True:
+                if user.time_check() is True:
                     if (user.sleep_to - current_hour) % 3 == 0:
-                        if current_hour == user.sleep_from:
+                        if current_hour == user.sleep_from or user.sleep_time_check():
                             try:
                                 await bot.send_sticker(chat_id=user.id, sticker=random.choice(stickers_good_night))
                             except Exception as e:
-                                self.logging(e, user)
+                                user.logging(e)
                         elif current_hour == user.sleep_to:
                             try:
                                 await bot.send_sticker(chat_id=user.id, sticker=random.choice(stickers_good_morning))
                             except Exception as e:
-                                self.logging(e, user)
+                                user.logging(e)
                         msg_word = f'<b>{user.word.word}</b> 🇬🇧 - <b>{user.word.translation}</b> 🇷🇺'
                         msg_context = f'{user.word.context_en} 🇬🇧\n' \
                                       f'{user.word.context_ru} 🇷🇺'
@@ -165,32 +207,4 @@ class Notificator:
                             await bot.send_message(chat_id=user.id, text=msg_word, parse_mode='HTML')
                             await bot.send_message(chat_id=user.id, text=msg_context, parse_mode='HTML')
                         except Exception as e:
-                            self.logging(e, user)
-
-    @staticmethod
-    def time_check(user: User) -> bool:
-        """
-        Checking if current time is True or False to send message
-        """
-        current_hour = time.gmtime(time.time() + (3600 * user.time_zone)).tm_hour
-        if user.sleep_from > user.sleep_to:
-            if current_hour in range(user.sleep_to, user.sleep_from + 1):
-                return True
-            else:
-                return False
-        else:
-            if current_hour not in range(user.sleep_from + 1, user.sleep_to):
-                return True
-            else:
-                return False
-
-    @staticmethod
-    def logging(text: Exception, user: User):
-        with open('logs.txt', 'a') as logs:
-            logs.write(f'[{time.strftime("%H:%M", time.gmtime(time.time() + (3600 * 3)))}] {text} for {user.first_name} [{user.sleep_from}-{user.sleep_to}, {user.period}]\n')
-
-
-
-
-        
-
+                            user.logging(e)
